36d214b138b5a45099d0aeecc80b43ce4bc17849
gokgokay <gokgokay35@gmail.com>
2021-08-30 21:14:15 +0300

    Added category model and administration site

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from book.models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
--- book/models.py
+++ book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
