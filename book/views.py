395a3ff3dffc16ed55c5349dd05c1c7453eecfd2
gokgokay <gokgokay35@gmail.com>
2021-09-16 16:27:29 +0300

    Added my own permissions.py and applied them to views.py

--- book/views.py
+++ book/views.py
@@ -1,44 +1,52 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
-
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from .models import Category, Author, Book, Comment
 from .serializers import CategorySerializer, AuthorSerializer, BookSerializer, CommentSerializer
-from rest_framework import generics
+from book.permissions import IsAdminUserOrReadOnly, IsUserOrReadOnly
 
 
 class CategoryCreateList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
 
 
 class AuthorCreateList(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAdminUser]
 
 
 class BookCreateList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
 
 
 class CommentCreate(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         book_pk = self.kwargs.get('book_pk')
@@ -53,3 +61,4 @@ class CommentCreate(generics.CreateAPIView):
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsUserOrReadOnly]
