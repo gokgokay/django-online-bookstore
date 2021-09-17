ac9e12a1bbcc19a301fa8aec37e46c3f1036e902
gokgokay <gokgokay35@gmail.com>
2021-09-17 23:31:27 +0300

    Added model of user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
