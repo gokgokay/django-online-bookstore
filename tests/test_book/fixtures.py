8ae19b22eb8222084a2f708253dbca7f200b3ad4
gokgokay <gokgokay35@gmail.com>
2021-11-16 04:23:03 +0300

    Added fixtures of category, author, book, user and comment models

--- tests/test_book/fixtures.py
+++ tests/test_book/fixtures.py
@@ -1,18 +1,57 @@
 import factory
-import pytest
-
+from faker import Factory
+from django.contrib.auth.models import User
 from book.models import Category, Author, Book, Comment
 
-
-@pytest.fixture
-def category_factory(session):
-    class CategoryFactory(factory.django.DjangoModelFactory):
-        class Meta:
-            model = Category
-
-        name = factory.Faker("word")
-        slug = factory.Faker("slug")
-
-    return CategoryFactory
+faker = Factory.create()
 
 
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = faker.word()
+    slug = faker.slug()
+
+
+class AuthorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Author
+
+    name = faker.word()
+    slug = faker.slug()
+    bio = faker.text()
+
+
+class BookFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    categories = factory.SubFactory(CategoryFactory)
+    authors = factory.SubFactory(AuthorFactory)
+    name = faker.word()
+    slug = faker.slug()
+    price = faker.random_int()
+    stock = faker.random_int()
+    available = faker.boolean()
+    description = faker.text()
+    language = faker.word()
+    image = faker.image_url()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = faker.name()
+    password = faker.password()
+
+
+class CommentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Comment
+
+    books = factory.SubFactory(BookFactory)
+    users = factory.SubFactory(UserFactory)
+    comment = faker.text()
+    rate = faker.random_int()
