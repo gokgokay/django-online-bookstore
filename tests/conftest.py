ef20e45d714db0fa9d2b0fb36f7b18e87814e069
gokgokay <gokgokay35@gmail.com>
2021-11-16 04:25:02 +0300

    Added some base fixtures and registered factories

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+from faker import Factory
+from pytest_factoryboy import register
+from django.test.client import Client
+from tests.test_book.fixtures import AuthorFactory, BookFactory, CategoryFactory, CommentFactory, UserFactory
+
+faker = Factory.create()
+
+register(AuthorFactory)
+register(BookFactory)
+register(CategoryFactory)
+register(CommentFactory)
+register(UserFactory)
+
+
+@pytest.fixture()
+def user(db, django_user_model, django_username_field):
+    UserModel = django_user_model
+    username_field = django_username_field
+
+    try:
+        user = UserModel._default_manager.get(**{username_field: faker.name()})
+    except UserModel.DoesNotExist:
+        user = UserModel._default_manager.create_user(
+            faker.name(), faker.password())
+    return user
+
+
+@pytest.fixture()
+def admin_user(db, django_user_model, django_username_field):
+    user_model = django_user_model
+    username_field = django_username_field
+
+    try:
+        user = user_model._default_manager.get(**{username_field: faker.name()})
+    except user_model.DoesNotExist:
+        user = user_model._default_manager.create_superuser(
+            faker.name(), faker.password(), faker.email())
+    return user
+
+
+@pytest.fixture()
+def admin_client(db, admin_user):
+    client = Client().force_login(admin_user)
+    return client
+
+
+@pytest.fixture()
+def user_client(db, user):
+    client = Client().force_login(user)
+    return client
