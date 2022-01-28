f6707176d0bd7e215ed8128e4a1040d2b0669938
gokgokay <gokgokay35@gmail.com>
2022-01-28 22:38:06 +0300

    Refactored serializers and models

--- profile/serializers.py
+++ profile/serializers.py
@@ -4,10 +4,11 @@ from rest_framework import serializers
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
+    follows = serializers.CharField(source='user.username')
+    favorites = serializers.CharField(source='book.name')
     bio = serializers.CharField(allow_blank=True, required=False)
-    image = serializers.ImageField(read_only=True)
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image')
+        fields = ('username', 'follows', 'favorites', 'bio')
         read_only_fields = ('username',)
