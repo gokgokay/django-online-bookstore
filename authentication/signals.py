470b3dc55a80b67528c15a181144e89ef1461307
gokgokay <gokgokay35@gmail.com>
2022-02-02 04:15:57 +0300

    Moved below class authentication and deleted unnecessary libraries

--- authentication/signals.py
+++ authentication/signals.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from profile.models import Profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-from django.conf import settings
 
 
 @receiver(post_save, sender=User)
@@ -12,7 +10,3 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
