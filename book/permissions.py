49664b7a5de0e5bae496d12cc903f180f0972ff1
gokgokay <gokgokay35@gmail.com>
2022-02-10 14:27:38 +0300

    Added IsOwnerOrReadOnly permission

--- book/permissions.py
+++ book/permissions.py
@@ -1,14 +1,9 @@
 from rest_framework import permissions
 
 
-class IsAdminUserOrReadOnly(permissions.IsAdminUser):
-    def has_permission(self, request, view):
-        is_admin = super().has_permission(request, view)
-        return request.method in permissions.SAFE_METHODS or is_admin
+class IsOwnerOrReadOnly(permissions.BasePermission):
 
-
-class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user == obj.users
+        return obj.user.id == request.user.id
