36458097f70e78d5f8998254ce0ab7d63ea6b09b
gokgokay <gokgokay35@gmail.com>
2022-02-10 06:40:32 +0300

    Added url for CommentListCreateAPIView

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import CategoryListAPIView, AuthorListAPIView, BookListAPIView, CommentListAPIView, CommentsDestroyAPIView
+from .views import CategoryListAPIView,  AuthorListAPIView, BookListAPIView, CommentsListCreateAPIView
 
 router = DefaultRouter()
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('category/', CategoryListAPIView.as_view(), name='categories'),
     path('author/', AuthorListAPIView.as_view(), name='authors'),
     path('book/', BookListAPIView.as_view(), name='books'),
-    path('comment/', CommentListAPIView.as_view(), name='comments'),
+    path('book/<book_slug>/comment/', CommentsListCreateAPIView.as_view(), name='comment'),
 ]
