567fab4d3c2de7d8df73f30831a7a4c9d9f06441
gokgokay <gokgokay35@gmail.com>
2022-02-17 23:15:21 +0300

    Deleted unused libraries

--- profile/views.py
+++ profile/views.py
@@ -1,13 +1,5 @@
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import serializers, status, generics, mixins
-from rest_framework.exceptions import NotFound
-from rest_framework.generics import RetrieveAPIView, GenericAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from rest_framework.viewsets import GenericViewSet
-from .models import Profile
 from .serializers import ProfileSerializer
 
 
