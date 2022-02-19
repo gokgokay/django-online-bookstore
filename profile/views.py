9bdc54b82edef3728b115a4b74934efe04ec5a5e
gokgokay <gokgokay35@gmail.com>
2022-02-19 20:03:47 +0300

    Added blank version of list of profile view

--- profile/views.py
+++ profile/views.py
@@ -1,14 +1,21 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView
 from .serializers import ProfileSerializer
 
 
+class ListProfileView(ListAPIView):
+    pass
+
+
 class ProfileFollowAPIView(APIView):
-    permission_classes = (IsAuthenticated,)
     serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def __delete__(self, instance):
+        pass
 
     def post(self, request, username=None):
         pass
 
-    def delete(self, request, username=None):
-        pass
