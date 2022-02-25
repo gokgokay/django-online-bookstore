45ec868ba0f4577620534042a85a6a4d5f272dbc
gokgokay <gokgokay35@gmail.com>
2022-02-25 23:34:05 +0300

    Refactored get_object method for update and retrieve profile view

--- profile/views.py
+++ profile/views.py
@@ -1,8 +1,12 @@
+from rest_framework import status
 from rest_framework.exceptions import NotFound
-from rest_framework.generics import RetrieveAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.generics import RetrieveAPIView, ListAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 from .models import Profile
+from .permissions import IsOwnerOrReadOnly
 from .serializers import ProfileSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 
 class ListProfileView(ListAPIView):
@@ -10,3 +14,13 @@ class ListProfileView(ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     ordering_fields = 'timestamp'
+
+
+class UpdateRetrieveProfileView(RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly, AllowAny)
+
+    def get_object(self):
+        return get_object_or_404(self.get_queryset(), user__username=self.kwargs.get('username'))
+    
