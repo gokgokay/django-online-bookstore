a8060e0ad9a9311b4df156c64fc2ff82f7d5d028
gokgokay <gokgokay35@gmail.com>
2022-02-27 23:29:28 +0300

    Addded urls for UpdateRetrieveProfileView

--- profile/urls.py
+++ profile/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from profile.views import ListProfileView
+from profile.views import ListProfileView, UpdateRetrieveProfileView
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
     path('profiles/', ListProfileView.as_view()),
+    path('profiles/<str:username>/', UpdateRetrieveProfileView.as_view()),
 ]
