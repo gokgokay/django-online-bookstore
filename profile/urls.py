579dd766eae772d1599a2ea3e2013b322a3d2457
gokgokay <gokgokay35@gmail.com>
2022-03-02 23:53:38 +0300

    Added url for ProfileFollowAPIView

--- profile/urls.py
+++ profile/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from profile.views import ListProfileView, UpdateRetrieveProfileView
+from profile.views import ListProfileView, RetrieveUpdateProfileView, ProfileFollowAPIView
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
     path('profiles/', ListProfileView.as_view()),
-    path('profiles/<str:username>/', UpdateRetrieveProfileView.as_view()),
+    path('profiles/<str:username>/', RetrieveUpdateProfileView.as_view()),
+    path('profiles/<str:username>/follow/', ProfileFollowAPIView.as_view())
 ]
