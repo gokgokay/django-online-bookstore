e8238af17436c775017ad572ec114d609b47dac7
gokgokay <gokgokay35@gmail.com>
2022-03-21 02:09:04 +0300

    Added serializer test of language and muted signals of comment model

--- tests/test_book/test_serializers.py
+++ tests/test_book/test_serializers.py
@@ -1,4 +1,6 @@
-from book.serializers import CategorySerializer, AuthorSerializer, BookSerializer, CommentSerializer
+import factory
+from django.db.models import signals
+from book.serializers import CategorySerializer, AuthorSerializer, BookSerializer, CommentSerializer, LanguageSerializer
 
 
 def test_serializer_category(db, category_factory, check):
@@ -9,6 +11,16 @@ def test_serializer_category(db, category_factory, check):
     }
     expected_category = CategorySerializer(data=serializer_category)
     check.equal(True, expected_category.is_valid(raise_exception=True))
+
+
+def test_serializer_language(db, language_factory, check):
+    language = language_factory()
+    serializer_language = {
+        'id': language.id,
+        'name': language.name,
+    }
+    expected_language = LanguageSerializer(data=serializer_language)
+    check.equal(True, expected_language.is_valid(raise_exception=True))
 
 
 def test_serializer_author(db, author_factory, check):
@@ -22,14 +34,14 @@ def test_serializer_author(db, author_factory, check):
     check.equal(True, expected_author.is_valid(raise_exception=True))
 
 
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
 def test_serializer_comment(db, comment_factory, check):
     comment = comment_factory()
     serializer_comment = {
         'id': comment.id,
-        'books': comment.books.id,
-        'users': comment.users.id,
-        'comment': comment.comment,
-        'rate': comment.rate
+        'book': comment.book.id,
+        'profile': comment.profile.id,
+        'body': comment.body,
     }
     expected_comment = CommentSerializer(data=serializer_comment)
     check.equal(True, expected_comment.is_valid(raise_exception=True))
@@ -44,8 +56,8 @@ def test_serializer_book(db, book_factory, check):
         'available': book.available,
         'description': book.description,
         'language': book.language,
-        'categories': book.categories.id,
-        'authors': book.authors.id
+        'category': book.category.id,
+        'author': book.author.id
     }
     expected_book = BookSerializer(data=serializer_book)
     check.equal(True, expected_book.is_valid(raise_exception=True))
