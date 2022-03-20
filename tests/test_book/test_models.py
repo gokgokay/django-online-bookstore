f97989101b9cedeb1d0dfa40f917c9dfadae3f94
gokgokay <gokgokay35@gmail.com>
2022-03-21 02:07:49 +0300

    Added str test of language and muted signals for comment model

--- tests/test_book/test_models.py
+++ tests/test_book/test_models.py
@@ -1,6 +1,15 @@
+import factory
+from django.db.models import signals
+
+
 def test_category_str(db, category_factory, check):
     category = category_factory.create()
     check.is_(category.__str__(), f"{category.name}")
+
+
+def test_language_str(db, language_factory, check):
+    language = language_factory.create()
+    check.is_(language.__str__(), f"{language.name}")
 
 
 def test_author_str(db, author_factory, check):
@@ -13,11 +22,7 @@ def test_book_str(db, book_factory, check):
     check.is_(book.__str__(), f"{book.name}")
 
 
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
 def test_comment_str(db, comment_factory, check):
     comment = comment_factory.create()
-    check.equal(comment.__str__(), f"{comment.comment}")
-
-
-def test_user_str(db, user_factory, check):
-    user = user_factory.create()
-    check.is_(user.__str__(), f"{user.username}")
+    check.equal(comment.__str__(), f"{comment.body}")
