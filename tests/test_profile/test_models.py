01dd8e9c372572efc3fa8af6593c43165cd0c28d
gokgokay <gokgokay35@gmail.com>
2022-04-03 23:38:44 +0300

    Added testing of profile testing models as stantard version

--- tests/test_profile/test_models.py
+++ tests/test_profile/test_models.py
@@ -11,3 +11,33 @@ def test_profile_str(db, profile_factory, check):
 def test_user_str(db, user_factory, check):
     user = user_factory.create()
     check.is_(user.__str__(), f"{user.username}")
+
+
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
+def test_profile_follow(db, profile_factory, check):
+    pass
+
+
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
+def test_profile_unfollow(db, profile_factory, check):
+    pass
+
+
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
+def test_profile_is_followed_by(db, profile_factory, check):
+    pass
+
+
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
+def test_profile_favorite(db, profile_factory, check):
+    pass
+
+
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
+def test_profile_unfavorite(db, profile_factory, check):
+    pass
+
+
+@factory.django.mute_signals(signals.pre_save, signals.post_save)
+def test_profile_has_favorited(db, profile_factory, check):
+    pass
