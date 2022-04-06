fb4610c1c4d4b4e580d419b6df0e993ffbf8bb64
gokgokay <gokgokay35@gmail.com>
2022-04-06 18:51:18 +0300

    Added some tests of profile models

--- tests/test_profile/test_models.py
+++ tests/test_profile/test_models.py
@@ -15,12 +15,29 @@ def test_user_str(db, user_factory, check):
 
 @factory.django.mute_signals(signals.pre_save, signals.post_save)
 def test_profile_follow(db, profile_factory, check):
-    pass
+    follower = profile_factory()
+    followee = profile_factory()
+    follower.follow(followee)
+    check.is_(1, follower.follows.count())
+
+    followee2 = profile_factory()
+    follower.follow(followee2)
+    check.is_(2, follower.follows.count())
 
 
 @factory.django.mute_signals(signals.pre_save, signals.post_save)
 def test_profile_unfollow(db, profile_factory, check):
-    pass
+    follower = profile_factory()
+    followee = profile_factory()
+    followee2 = profile_factory()
+    follower.follow(followee)
+    follower.follow(followee2)
+
+    follower.unfollow(followee)
+    check.is_(1, follower.follows.count())
+
+    follower.unfollow(followee2)
+    check.is_(0, follower.follows.count())
 
 
 @factory.django.mute_signals(signals.pre_save, signals.post_save)
@@ -29,13 +46,30 @@ def test_profile_is_followed_by(db, profile_factory, check):
 
 
 @factory.django.mute_signals(signals.pre_save, signals.post_save)
-def test_profile_favorite(db, profile_factory, check):
-    pass
+def test_profile_favorite(db, profile_factory, book_factory, check):
+    profile = profile_factory()
+    book = book_factory()
+    profile.favorite(book)
+    check.is_(1, profile.favorite_books.count())
+
+    book2 = book_factory()
+    profile.favorite(book2)
+    check.is_(2, profile.favorite_books.count())
 
 
 @factory.django.mute_signals(signals.pre_save, signals.post_save)
-def test_profile_unfavorite(db, profile_factory, check):
-    pass
+def test_profile_unfavorite(db, profile_factory, book_factory, check):
+    profile = profile_factory()
+    book = book_factory()
+    book2 = book_factory()
+    profile.favorite(book)
+    profile.favorite(book2)
+
+    profile.unfavorite(book)
+    check.is_(1, profile.favorite_books.count())
+
+    profile.unfavorite(book2)
+    check.is_(0, profile.favorite_books.count())
 
 
 @factory.django.mute_signals(signals.pre_save, signals.post_save)
