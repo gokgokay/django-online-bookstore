514e781c89561c5d3a0490085df91864f1f3a767
gokgokay <gokgokay35@gmail.com>
2022-04-07 15:04:07 +0300

    Added testing of LanguageListAPIView

--- tests/test_book/test_views.py
+++ tests/test_book/test_views.py
@@ -9,3 +9,13 @@ class TestCategoryListAPIView:
         response = api_client().get(self.endpoint)
         check.is_(200, response.status_code)
         check.is_(5, len(json.loads(response.content)['categories']))
+
+
+class TestLanguageListAPIView:
+    endpoint = '/api/language/'
+
+    def test_list(self, db, api_client, language_factory, check):
+        language = language_factory.create_batch(5)
+        response = api_client().get(self.endpoint)
+        check.is_(200, response.status_code)
+        check.is_(5, len(json.loads(response.content)['languages']))
