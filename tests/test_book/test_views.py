aabd729e684486a6b3de271bc6d69663b09b78b5
gokgokay <gokgokay35@gmail.com>
2022-04-07 14:57:49 +0300

    Added testing of CategoryListAPIView

---
+++ tests/test_book/test_views.py
@@ -0,0 +1,11 @@
+import json
+
+
+class TestCategoryListAPIView:
+    endpoint = '/api/category/'
+
+    def test_list(self, db, api_client, category_factory, check):
+        category = category_factory.create_batch(5)
+        response = api_client().get(self.endpoint)
+        check.is_(200, response.status_code)
+        check.is_(5, len(json.loads(response.content)['categories']))
