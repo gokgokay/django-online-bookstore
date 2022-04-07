3b4bb20c466488c812d4c482d523aeb461b24725
gokgokay <gokgokay35@gmail.com>
2022-04-07 15:06:42 +0300

    Added testing of BookListAPIView

--- tests/test_book/test_views.py
+++ tests/test_book/test_views.py
@@ -19,3 +19,23 @@ class TestLanguageListAPIView:
         response = api_client().get(self.endpoint)
         check.is_(200, response.status_code)
         check.is_(5, len(json.loads(response.content)['languages']))
+
+
+class TestAuthorListAPIView:
+    endpoint = '/api/author/'
+
+    def test_list(self, db, api_client, author_factory, check):
+        author = author_factory.create_batch(5)
+        response = api_client().get(self.endpoint)
+        check.is_(200, response.status_code)
+        check.is_(5, len(json.loads(response.content)['authors']))
+
+
+class TestBookListAPIView:
+    endpoint = '/api/book/'
+
+    def test_list(self, db, api_client, book_factory, check):
+        book = book_factory.create_batch(5)
+        response = api_client().get(self.endpoint)
+        check.is_(200, response.status_code)
+        check.is_(5, len(json.loads(response.content)['books']))
