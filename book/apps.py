9fcdd5ef7872a4f9c5a5ef48d696a6327e7c5649
gokgokay <gokgokay35@gmail.com>
2022-05-10 03:00:59 +0300

    Fixed error of auto creating profile

--- book/apps.py
+++ book/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'book'
+
+    # for signals.property
+    def ready(self):
+        pass
